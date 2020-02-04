6b346683db63e5ebf45639d58803cad2ce56af91
Alessandro Elias <ale.elias2011@gmail.com>
2020-02-04 20:15:16 -0300

    Add cmdline parameters
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- server.js
+++ server.js
@@ -21,9 +21,59 @@
  * USA.
  */
 
+
 var express = require('express');
+var os = require('os');
+var path = require('path');
+var minimist = require('minimist');
 var app = express();
 
+
+//
+// Parse command line arguments
+//
+var argv = minimist(process.argv.slice(2));
+if (argv.help || argv.h) {
+    console.log('Usage: server.js [OPTION]...');
+    console.log('');
+    console.log('Where OPTION can be:');
+    console.log('    -c, --config       alternative configuration file (default: config.js)');
+    console.log('    -p, --port         port where the server will listen to');
+    console.log('    --dev              force development mode');
+    console.log('    -n, --num-workers  number of worker processes to spawn');
+    console.log('    -d, --daemon       start the server as a daemon');
+    console.log('    --pid-file         path for the pid file when running as a daemon');
+    console.log('    --stdout-log       output log on standard output');
+    console.log('    -h, --help         show this help and exit');
+    return 0;
+}
+
+var configFile = argv.config || argv.c || 'config.js';
+var config;
+try {
+    config = require(path.resolve('.', configFile));
+} catch( e ) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+        console.error('No ' + configFile + ', fallback to default values.\n');
+        config = {};
+    }
+    else {
+        console.error('Error while parsing configuration file ('+configFile+')!\n');
+        console.error(e.stack);
+        return 2;
+    }    
+}
+
+var devMode = argv.dev || 
+    (process.env.NODE_ENV && (process.env.NODE_ENV.slice(0,3) === 'dev')) || 'dev';
+var port = parseInt(argv.port || argv.p || process.env.PORT) || 3000;
+var numWorkers = parseInt(argv['num-workers'] || argv.n) || (os.cpus().length);
+var daemon = argv.daemon || argv.d || false;
+var pidfile = argv['pid-file'] || config.pid_file || process.env.PID_FILE || path.resolve('/run/', 'webserver.pid');
+var stdoutLog = argv['stdout-log'] || false;
+
+console.log('param=',devMode, port, numWorkers, daemon, pidfile, stdoutLog);
+return 0;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
