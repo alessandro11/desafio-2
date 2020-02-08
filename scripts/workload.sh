28cfd54a5f3003d11c0e287414fbb910bd0889ae
Alessandro Elias <ale.elias2011@gmail.com>
2020-02-08 16:57:08 -0300

    Classify errors to curl_requests_failed.log file
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- scripts/workload.sh
+++ scripts/workload.sh
@@ -2,18 +2,24 @@
 
 nworkers=${1:-100}
 domain=${2:-http://webserver.com/}
-pids=""
-failed=0
 
+aux=""
+code=0
 res_time="$(curl -s -o /dev/null -m 3 -w "%{http_code} %{time_starttransfer}" "$domain" &)"
 for ((i=0; i < $nworkers-1; i++ )); do
-    res_time="${res_time},$(curl -s -o /dev/null -m 3 -w "%{http_code} %{time_starttransfer}" "$domain" &)"
+    aux="$(curl -s -o /dev/null -m 1 -w "%{http_code} %{time_starttransfer}" "$domain" &)"
+
+    read -d ' ' code < <(echo $aux)
+    if [ "x$code" != "x200" ]; then
+        echo "$aux" >> curl_requests_failed.log
+    else
+        res_time="$res_time\n$aux"
+    fi
 done
 
 wait
 
-echo "$res_time"
-echo "Failed: $failed"
+echo -e "$res_time"
 
 exit 0
 
