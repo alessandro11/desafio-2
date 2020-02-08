b1df7ebf6c42311d00126596a6f44a552e68b046
Alessandro Elias <ale.elias2011@gmail.com>
2020-02-08 00:23:03 -0300

    Set default branch for deploy
    Signed-off-by: Alessandro Elias <ale.elias2011@gmail.com>

--- shipitfile.js
+++ shipitfile.js
@@ -1,22 +1,23 @@
 module.exports = shipit => {
-  require('shipit-deploy')(shipit)
- 
-  shipit.initConfig({
-    default: {
-      deployTo: '/home/webserver/',
-      repositoryUrl: 'https://github.com/alessandro11/desafio-2.git',
-      keepReleases: 10,
-      keepWorkspace: false, // should we remove workspace dir after deploy?
-      deleteOnRollback: false,
-      //key: '~/.ssh/id_ecdsa',
-      deploy: {
-        remoteCopy: {
-          copyAsDir: false, // Should we copy as the dir (true) or the content of the dir (false)
+    require('shipit-deploy')(shipit)
+
+    shipit.initConfig({
+        default: {
+            deployTo: '/home/webserver/',
+            repositoryUrl: 'https://github.com/alessandro11/desafio-2.git',
+            keepReleases: 10,
+            keepWorkspace: false, // should we remove workspace dir after deploy?
+            deleteOnRollback: false,
+            //key: '~/.ssh/id_ecdsa',
+            branch: master
+            deploy: {
+                remoteCopy: {
+                    copyAsDir: false, // Should we copy as the dir (true) or the content of the dir (false)
+                },
+            },
         },
-      },
-    },
-    production: {
-      servers: 'webserver@webserver.com',
-    },
-  })
+        production: {
+            servers: 'webserver@webserver.com',
+        },
+    })
 }
